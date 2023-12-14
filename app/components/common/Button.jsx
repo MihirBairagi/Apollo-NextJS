3512fc30b19f159f697e074d72cc6b79cfd27573
MihirBairagi <mihirbairagi10@gmail.com>
2023-12-14 14:33:36 +0530

    fixed

--- app/components/common/Button.jsx
+++ app/components/common/Button.jsx
@@ -1,13 +1,13 @@
+import React from "react";
 import Image from "next/image";
-
 
 const Button = ({ buttonText }) => {
   return (
     <div className="btn-box bg-orange hover:bg-[#e55e02] w-max text-white rounded-[0.3rem]">
-      <a href="" className="flex items-center justify-between py-[1rem] px-[1.5rem]">
-        <p className="uppercase font-[600] tracking-[.15em]">
-          {buttonText}
-        </p>
+      <a 
+        href="" 
+        className="flex items-center justify-between py-[1rem] px-[1.5rem]">
+        <p className="uppercase font-[600] tracking-[.15em]">{buttonText}</p>
         <span className="w-[1.9rem] ml-[1rem]">
           <Image
             src="/assets/img/arrow-2.png"
@@ -19,7 +19,5 @@ const Button = ({ buttonText }) => {
     </div>
   );
 };
-
-
 
 export default Button;
