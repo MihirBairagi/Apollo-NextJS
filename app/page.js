1c9847b234160766856402022654fbdd5b9fb99b
MihirBairagi <mihirbairagi10@gmail.com>
2023-12-14 15:52:18 +0530

    fixed

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-import Image from 'next/image'
+import React from 'react'
 import BannerSection from './components/BannerSection'
 import PageUSP from './components/PageUSP'
 import AboutCompany from './components/AboutCompany'
